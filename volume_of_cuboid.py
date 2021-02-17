2d6ec237ab1bd4582574c734fae0fa48462db74c
ShashikantBhargav <75611743+ShashikantBhargav@users.noreply.github.com>
2021-02-17 22:16:57 +0530

    Add files via upload

---
+++ volume_of_cuboid.py
@@ -0,0 +1,7 @@
+print("enter length, breath and height of cuboid:")
+l=float(input("enter length:"))
+b=float(input("enter breath:"))
+h=float(input("enter height:"))
+
+v=l*b*h
+print("volume is:",v)
